c78c292f6ae9161e21f992ee573250de1503a14f
Danang Rusmanto <danang@roots.co.id>
2021-02-06 18:59:10 +0700

    update

--- app/models/personil.js
+++ app/models/personil.js
@@ -31,6 +31,7 @@ const Personil = function (personil) {
     this.action = personil.action;
     this.user_id = personil.user_id;
     this.remark = personil.remark;
+    this.koneksi = personil.koneksi;
 };
 
 Personil.create = async (newPersonil, result) => {
@@ -58,12 +59,14 @@ Personil.create = async (newPersonil, result) => {
         obj.action = newPersonil.action;
         obj.user_id = newPersonil.user_id;
         obj.remark = newPersonil.remark;
+        obj.koneksi = newPersonil.koneksi;
         await query("INSERT INTO activity_log SET ?", obj);
         delete newPersonil.date;
         delete newPersonil.item;
         delete newPersonil.action;
         delete newPersonil.user_id;
         delete newPersonil.remark;
+        delete newPersonil.koneksi;
 
         const res = await query("INSERT INTO personil SET ?", newPersonil);
         result(null, { id: res.insertId, ...newPersonil });
@@ -92,7 +95,7 @@ Personil.findById = (id, result) => {
 
 Personil.getAll = (param, result) => {
     const length = Object.keys(param).length;
-    var query = "SELECT a.* , a1.nama as tipe_personil, a2.nama as approval_status, a3.nama as enable, a4.nama_asset as asset_kapal, a5.nama as status_kepegawaian FROM personil a LEFT JOIN tipe_personil a1 ON a.tipe_personil_id = a1.id  LEFT JOIN approval_status a2 ON a.approval_status_id = a2.id  LEFT JOIN enable a3 ON a.enable = a3.id  LEFT JOIN asset_kapal a4 ON a.asset_kapal_id = a4.id  LEFT JOIN status_kepegawaian a5 ON a.status_kepegawaian_id = a5.id ";
+    var query = "SELECT a.* , a1.nama as tipe_personil, a2.nama as approval_status, a3.nama as ena, a4.nama_asset as asset_kapal, a5.nama as status_kepegawaian FROM personil a LEFT JOIN tipe_personil a1 ON a.tipe_personil_id = a1.id  LEFT JOIN approval_status a2 ON a.approval_status_id = a2.id  LEFT JOIN enable a3 ON a.enable = a3.id  LEFT JOIN asset_kapal a4 ON a.asset_kapal_id = a4.id  LEFT JOIN status_kepegawaian a5 ON a.status_kepegawaian_id = a5.id ";
     if (length > 0) {
         query += " WHERE ";
         for (var i in param) {
@@ -101,12 +104,12 @@ Personil.getAll = (param, result) => {
             if (typeof str != "string") {
                 query += "(";
                 for (var x in str) {
-                    query += i + " ='" + str[x] + "' or ";
+                    query += "a." + i + " ='" + str[x] + "' or ";
                 }
                 query = query.substring(0, query.length - 4);
                 query += ") and ";
             } else {
-                query += i + " ='" + param[i] + "' and ";
+                query += "a." + i + " ='" + param[i] + "' and ";
             }
         }
 
@@ -161,12 +164,14 @@ Personil.updateById = async (id, personil, result) => {
         obj.action = Personil.action;
         obj.user_id = Personil.user_id;
         obj.remark = Personil.remark;
+        obj.koneksi = Personil.koneksi;
         await query("INSERT INTO activity_log SET ?", obj);
         delete Personil.date;
         delete Personil.item;
         delete Personil.action;
         delete Personil.user_id;
         delete Personil.remark;
+        delete Personil.koneksi;
 
 
         var str = "", obj = [], no = 1;

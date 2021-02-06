99a3af0b72a64f0d2ae6c8200b0912cb7c1cc7e4
Danang Rusmanto <danang@roots.co.id>
2021-02-06 16:28:41 +0700

    update

--- app/models/personil.js
+++ app/models/personil.js
@@ -101,17 +101,18 @@ Personil.getAll = (param, result) => {
             if (typeof str != "string") {
                 query += "(";
                 for (var x in str) {
-                    query += i + " ='" + str[x] + "' or ";
+                    query += "a." + i + " ='" + str[x] + "' or ";
                 }
                 query = query.substring(0, query.length - 4);
                 query += ") and ";
             } else {
-                query += i + " ='" + param[i] + "' and ";
+                query += "a." + i + " ='" + param[i] + "' and ";
             }
         }
 
         query = query.substring(0, query.length - 5);
     }
+    console.log(query);
     sql.query(query, (err, res) => {
         if (err) {
             console.log("error: ", err);

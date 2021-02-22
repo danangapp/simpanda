816dfd6ceb5fc5ce0af976f8dfb9da27b7f24264
Danang Rusmanto <danang@roots.co.id>
2021-02-22 19:29:38 +0700

    betulin sertifikat

--- app/models/personil.js
+++ app/models/personil.js
@@ -187,9 +187,9 @@ Personil.updateById = async (id, personil, result) => {
 
 			if (adadiTable == 1) {
 				header += a + ", ";
-				if (a === "tanggal_keluar_sertifikat" || a === "tanggal_expire" || a === "reminder_date1" || a === "reminder_date3" || a === "reminder_date6") {
-					val = f.toDate(val);
-				}
+				// if (a === "tanggal_keluar_sertifikat" || a === "tanggal_expire" || a === "reminder_date1" || a === "reminder_date3" || a === "reminder_date6") {
+				// 	val = f.toDate(val);
+				// }
 				value += "'" + val + "', ";
 			}
 		}

a97f78309e9ccd64050c6bab798b3d625a6b6529
Danang Rusmanto <danang@roots.co.id>
2021-03-01 19:57:07 +0700

    update pandu jaga

--- app/models/armadajaga.js
+++ app/models/armadajaga.js
@@ -80,7 +80,7 @@ ArmadaJaga.getAll = (param, result) => {
 						wheres += "a1." + i + " ='" + param[i] + "' and ";
 					} else if (i == "cabang_id" || i == "simop_kd_fas" || i == "kepemilikan_kapal" || i == "simop_status_milik" || i == "simop_kd_agen" || i == "nama_asset" || i == "horse_power" || i == "tahun_perolehan" || i == "nilai_perolehan" || i == "enable" || i == "asset_number" || i == "simop_kd_puspel_jai" || i == "simop_new_puspel_jai" || i == "simop_new_asset_jai" || i == "approval_status_id" || i == "loa" || i == "tahun_pembuatan" || i == "breadth" || i == "kontruksi" || i == "depth" || i == "negara_pembuat" || i == "draft_max" || i == "daya" || i == "putaran" || i == "merk" || i == "tipe" || i == "daya_motor" || i == "daya_generator" || i == "putaran_spesifikasi" || i == "merk_spesifikasi" || i == "tipe_spesifikasi" || i == "klas" || i == "notasi_permesinan" || i == "no_registrasi" || i == "notasi_perlengkapan" || i == "port_of_registration" || i == "notasi_perairan" || i == "notasi_lambung" || i == "gross_tonnage" || i == "bolard_pull" || i == "kecepatan" || i == "ship_particular" || i == "sertifikat_id") {
 						wheres += "a2." + i + " ='" + param[i] + "' and ";
-					} else if (i == "date" || i == "cabang" || i == "status" || i == "jam_pengoperasian" || i == "reliability" || i == "keterangan" || i == "armada_jaga_id") {
+					} else if (i == "date" || i == "cabang" || i == "status" || i == "jam_pengoperasian" || i == "reliability" || i == "keterangan" || i == "armada_jaga_id" || i == "asset_kapal_id") {
 						wheres += "a3." + i + " ='" + param[i] + "' and ";
 					} else {
 						wheres += "a." + i + " ='" + param[i] + "' and ";
